c7c0dd49dc2fbd6b2cbb802677aa7563ae9778d5
Khandakar Amir Hossain <amirkhondokar@gmail.com>
2023-01-15 21:15:48 +0600

    Add files via upload

---
+++ hello_amir.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]){
+printf("hello amir how are you\n");
+for(int i=0;i<argc;++i){
+    printf("%s\n",argv[i]);
+}
+return 0;
+}
+
